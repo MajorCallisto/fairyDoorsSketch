755abfabe9227757f1242a99aeb2e6e664846a07
Christopher Lewis <chris@gomangostudios.net>
2015-02-27 11:16:50 -0500

    Inverted door swith to fix broken lead

--- fairyDoors.ino
+++ fairyDoors.ino
@@ -1,6 +1,10 @@
 //Fairy Doors Sketch
 //by Christopher Lewis
-//
+//Creative Commons License
+//Fairy Doors Sketch by Christopher Lewis
+//is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 
+//4.0 International License.
+
 int pinDoor = 2;
 int lastDoorVal = LOW;
 long lastActivation = 0;
@@ -16,30 +20,25 @@ void loop(){
   int doorVal = digitalRead(pinDoor);
   long timeElapsed = millis()- lastActivation;
   if (timeElapsed > timeSpan){
-    Serial.print("Trigger");
-    Serial.println(timeSpan);
-    Serial.println(lastActivation);
-    Serial.println(timeElapsed);
-    Serial.println("");
     resetTimer();
     delay(10);
     doorVal = HIGH;
     Serial.print("1");
-    digitalWrite(13, HIGH);
+    digitalWrite(13, LOW);
     delay(100);
     Serial.print("0");
-    digitalWrite(13, LOW);
+    digitalWrite(13, HIGH);
   }
   /**/
   
   if (doorVal == HIGH && lastDoorVal != doorVal) {
-    digitalWrite(13, HIGH);
+    digitalWrite(13, LOW);
     //Door opend
     //Serial.println("Door opened");
     Serial.print("1");
     resetTimer();
   }  else if (doorVal == LOW && lastDoorVal != doorVal) {
-    digitalWrite(13, LOW);
+    digitalWrite(13, HIGH);
     //Door closed
     //Serial.println("Door closed");
     Serial.print("0");
